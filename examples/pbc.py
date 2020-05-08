727675f1ef3fc0b5c58ec9067cf6f486a3139d4a
evenmn <evenmn@student.matnat.uio.no>
2020-05-08 16:11:35 +0200

    Updated examples.

--- examples/pbc.py
+++ examples/pbc.py
@@ -1,8 +1,9 @@
-""" Pack 10000 water molecules with density 0.998 g/cm³ and 
-periodic boundary condition
+""" Pack 10000 water molecules and periodic boundary condition
 """
 
 from pack_water import PackWater
-
-packer = PackWater(nummol=10000, density=0.998)
-packer(outfile="water_10000mol_PBC.data", pbc=2.0)
+from pack_water.geometry import CubeGeometry
+    
+packer = PackWater()
+packer.append(CubeGeometry(0, 0, 0, 40, number=10000))
+packer(pbc=2.0, outfile="water_10000mol_PBC.out")
